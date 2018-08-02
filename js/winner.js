3ea2ad02ade7d2837ffb7578283f91b71a774aa7
Dequan Johnson <johnsondequan900@gmail.com>
2018-08-01 22:56:48 -0400

    Add click listener on restartButton in winner page

--- js/winner.js
+++ js/winner.js
@@ -1,5 +1,14 @@
+//Get the number moves and stars.
+//Display on the moves and stars elements in the winners page.
 const finalMoves = sessionStorage.getItem("moves");
 const finalStars = sessionStorage.getItem("stars");
 document.querySelector('#moves').textContent = finalMoves;
 document.querySelector('#stars').textContent = finalStars;
-console.log("Moves: " + finalMoves + " and " + "Stars: " + finalStars);
+
+//Get the restart button.
+//Add listener that waits for a click on the button to go back
+//to retry tghe game.
+const restartButton = document.querySelector('#restartButton');
+restartButton.addEventListener('click', function () {
+  window.location="index.html";
+});
